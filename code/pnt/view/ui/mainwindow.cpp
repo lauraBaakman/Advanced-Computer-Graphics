124eeb3ac06297b48afa8cb75a4ee49b2ea970bb
Rick van Veen <r.van.veen133@gmail.com>
2016-01-07 15:28:50 +0100

    Included sidebar into mainwindow, tried to fix the size of all the widgets...

--- code/pnt/view/ui/mainwindow.cpp
+++ code/pnt/view/ui/mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->centralWidgetLayout->setAlignment(Qt::AlignLeft);
+    ui->centralWidgetLayout->setAlignment(Qt::AlignTop);
 }
 
 MainWindow::~MainWindow()
--- code/pnt/view/ui/sidebar.cpp
+++ code/pnt/view/ui/sidebar.cpp
@@ -6,8 +6,6 @@ Sidebar::Sidebar(QWidget *parent) :
     ui(new Ui::Sidebar)
 {
     ui->setupUi(this);
-
-    // Don't know if this works... but we will see when we add it.
     ui->sidebarUiLayout->setAlignment(Qt::AlignTop);
 }
 
