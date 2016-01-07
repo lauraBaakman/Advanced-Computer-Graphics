67da810e81c4bc1676528060de13ce3b12588c2b
Rick van Veen <r.van.veen133@gmail.com>
2016-01-07 14:56:13 +0100

    Initial sidebar layout

--- code/pnt/view/ui/sidebar.cpp
+++ code/pnt/view/ui/sidebar.cpp
@@ -6,6 +6,9 @@ Sidebar::Sidebar(QWidget *parent) :
     ui(new Ui::Sidebar)
 {
     ui->setupUi(this);
+
+    // Don't know if this works... but we will see when we add it.
+    ui->sidebarUiLayout->setAlignment(Qt::AlignTop);
 }
 
 Sidebar::~Sidebar()
