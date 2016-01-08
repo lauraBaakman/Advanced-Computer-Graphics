20adace5acedf3832f19cfd89d752bd7fbe04dfa
Laura Baakman <laurabaakman@gmail.com>
2016-01-08 09:12:47 +0100

    Opening file correclt.

--- code/pnt/model/obj.cpp
+++ code/pnt/model/obj.cpp
@@ -1,7 +1,24 @@
 #include "obj.h"
 
-Obj::Obj()
+Obj::Obj(QFile *file)
 {
+    qDebug() << "Received file: " << file->fileName();
 
+    openFile(file);
+
+    //openFile
+
+    //processFile
+
+    //closeFile
+    //delete textstream
 }
 
+QTextStream* Obj::openFile(QFile *file) const
+{
+    if(!(file->open(QIODevice::ReadOnly))){
+        QMessageBox::critical(0, "Error", "The file " + file->fileName() + " could not be opened.\n\n" + file->errorString());
+        return nullptr;
+    }
+    return new QTextStream(file);
+}
--- code/pnt/model/obj.h
+++ code/pnt/model/obj.h
@@ -2,11 +2,33 @@
 #define OBJ_H
 
 #include <QObject>
+#include <QFile>
+#include <QVector3D>
+#include <QTextStream>
+#include <QMessageBox>
+
+#include <QDebug>
 
 class Obj
 {
 public:
-    Obj();
+    Obj(QFile *file);
+
+private:
+
+    QTextStream* openFile(QFile *file) const;
+
+//    class vertexPosition;
+//    class vertexNormal;
+//    class face;
+
+
+//    QList<vertexPosition*> vertexPositions;
 };
+
+//class Obj::vertexPosition : public QVector3D {
+//public:
+//    static fromString(QString string);
+//};
 
 #endif // OBJ_H
