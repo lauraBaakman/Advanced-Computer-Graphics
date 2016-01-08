39ec091d8555565bff839c1fd2592841a7628233
Laura Baakman <laurabaakman@gmail.com>
2016-01-08 17:53:42 +0100

    Create vertexPostions and normals.

--- code/pnt/model/mesh.cpp
+++ code/pnt/model/mesh.cpp
@@ -4,18 +4,16 @@ Mesh::Mesh(Obj *object, QObject *parent):
     QObject(parent)
 {
     addVertices(object);
-    addEdges(object);
+    qDebug() << vertexPositions;
+    qDebug() << vertexNormals;
 }
 
 void Mesh::addVertices(Obj *object)
 {
     QVector3D* position;
     QVector3D* normal;
-    Vertex* vertex;
 
     QMapIterator<unsigned int, Obj::VertexPosition *> iterator(object->getVertexPositions());
-
-    qDebug() << "in addVertices:" << &endl;
 
     while (iterator.hasNext()) {
         iterator.next();
@@ -25,27 +23,6 @@ void Mesh::addVertices(Obj *object)
 
         vertexNormals.insert(iterator.key(), *(dynamic_cast<QVector3D*>(object->getVertexNormals().value(iterator.key()))));
         normal = &(vertexNormals[iterator.key()]);
-
-        vertex = new Vertex(position, normal);
-
-        vertices.insert(iterator.key(), vertex);
-    }
-}
-
-void Mesh::addEdges(Obj *object)
-{
-    QMapIterator<unsigned int, Obj::Face*> iterator(object->getFaces());
-    Obj::Face* face;
-    while(iterator.hasNext()){
-        iterator.next();
-        Edge* edge;
-        face = iterator.value();
-        for(int i = 0, j = 1; i < 3; i++, j = (i + 1) % 3){
-            edge = new Edge(
-                        vertices.value(face->value(i)),
-                        vertices.value(face->value(j)));
-            edges.append(edge);
-        }
     }
 }
 
@@ -53,15 +30,7 @@ QDebug operator<<(QDebug stream, const Mesh &mesh)
 {
     stream << "Mesh[ "                                             <<  &endl
            << "  vertexPositions:   ["  << mesh.vertexPositions    <<  &endl
-           << "  vertexNormals:     ["  << mesh.vertexNormals      <<  &endl;
-    stream << "vertices: " << &endl;
-    for(auto i : mesh.vertices){
-        stream << *i;
-    }
-    stream << &endl;
-    for(auto i : mesh.edges){
-        stream << *i;
-    }
-    stream << &endl;
+           << "  vertexNormals:     ["  << mesh.vertexNormals      <<  &endl
+           << "]";
     return stream;
 }
--- code/pnt/model/mesh.h
+++ code/pnt/model/mesh.h
@@ -17,14 +17,11 @@ public:
 
     friend QDebug operator<<(QDebug stream, const Mesh &mesh);
 private:
-
-    QList<Vertex*> vertices;
-    QList<Edge*> edges;
+    QList<unsigned int> faces;
     QVector<QVector3D> vertexPositions;
     QVector<QVector3D> vertexNormals;
 
     void addVertices(Obj *object);
-    void addEdges(Obj *object);
 
 signals:
 
