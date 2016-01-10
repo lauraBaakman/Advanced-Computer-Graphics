bb6d47db14bb761940e111a536a61d6cf00ee03d
Rick van Veen <r.van.veen133@gmail.com>
2016-01-10 14:29:53 +0100

    Fixed a problem with using the wrong index

--- code/pnt/resources/shaders/evaluation.glsl
+++ code/pnt/resources/shaders/evaluation.glsl
@@ -16,7 +16,7 @@ struct pnPatch {
 };
 
 //Variable in
-layout (triangles) in;
+layout (triangles, fractional_odd_spacing, ccw) in;
 
 layout(location = 0) in vec3 tcsNormals[];
 layout(location = 3) in pnPatch tcsPatches[];
@@ -25,6 +25,7 @@ layout(location = 3) in pnPatch tcsPatches[];
 layout(location = 0) out vec3 tesNormal;
 //in vec3 vColor[3];
 
+// ###### GEOMETRY START ###### //
 vec3 b300 = gl_in[0].gl_Position.xyz;
 vec3 b030 = gl_in[1].gl_Position.xyz;
 vec3 b003 = gl_in[2].gl_Position.xyz;
@@ -37,23 +38,26 @@ vec3 b102 = vec3(tcsPatches[0].b102, tcsPatches[1].b102, tcsPatches[2].b102);
 vec3 b201 = vec3(tcsPatches[0].b201, tcsPatches[1].b201, tcsPatches[2].b201);
 
 vec3 b111 = vec3(tcsPatches[0].b111, tcsPatches[1].b111, tcsPatches[2].b111);
+// ###### GEOMETRY END ######## //
 
-vec3 n200 = tcsNormals[0];
-vec3 n020 = tcsNormals[1];
-vec3 n002 = tcsNormals[2];
+// ###### NORMALS START ###### //
+vec3 n200 = normalize(tcsNormals[0]);
+vec3 n020 = normalize(tcsNormals[1]);
+vec3 n002 = normalize(tcsNormals[2]);
 
 vec3 n110 = normalize(vec3(tcsPatches[0].n110, tcsPatches[1].n110, tcsPatches[2].n110));
 vec3 n011 = normalize(vec3(tcsPatches[0].n011, tcsPatches[1].n011, tcsPatches[2].n011));
 vec3 n101 = normalize(vec3(tcsPatches[0].n101, tcsPatches[1].n101, tcsPatches[2].n101));
+// ###### NORMALS END ######## //
 
 float u = gl_TessCoord.x;
-float u2 = pow(u, 2);
+float u2 = pow(u, 2.0);
 
 float v = gl_TessCoord.y;
-float v2 = pow(v, 2);
+float v2 = pow(v, 2.0);
 
 float w = gl_TessCoord.z;
-float w2 = pow(w, 2);
+float w2 = pow(w, 2.0);
 
 void interpolateGeometricComponent(){
         float u3 = u2 * u;
@@ -62,7 +66,7 @@ void interpolateGeometricComponent(){
     vec3 position = b300 * w3 + b030 * u3 + b003 * v3 +
         b210 * 3.0 * w2 * u +
         b120 * 3.0 * w * u2 +
-        b210 * 3.0 * w2 * v +
+        b201 * 3.0 * w2 * v +
         b021 * 3.0 * u2 * v +
         b102 * 3.0 * w * v2 +
         b012 * 3.0 * u * v2 +
@@ -71,8 +75,8 @@ void interpolateGeometricComponent(){
 }
 
 void interpolateNormalComponent(){
-    tesNormal = n200 * w2 + n020 * u2 + n002 * v2 +
-        n110 * w * u + n011 * u * v + n101 * w * v;
+    tesNormal = normalize(n200 * w2 + n020 * u2 + n002 * v2 +
+        n110 * w * u + n011 * u * v + n101 * w * v);
 }
 
 void main(void)
