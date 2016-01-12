07a05e10b132c40933c013e9f9a6f0ffab248020
Laura Baakman <laurabaakman@gmail.com>
2016-01-12 12:09:12 +0100

    Flat shading

--- code/pnt/resources/shaders/geometry.glsl
+++ code/pnt/resources/shaders/geometry.glsl
@@ -10,7 +10,26 @@ layout(location = 0) in vec3 tesNormal[];
 layout(location = 0) out vec3 gsPosition;
 layout(location = 1) out vec3 gsNormal;
 
-void passThrough(){
+void faceNormal(){
+    vec3 edge1 = gl_in[1].gl_Position.xyz - gl_in[0].gl_Position.xyz;
+    vec3 edge2 = gl_in[2].gl_Position.xyz - gl_in[0].gl_Position.xyz;
+
+    vec3 meanNormal = (tesNormal[0] + tesNormal[1] + tesNormal[2]) / 3.0;
+    gsNormal = normalize(cross(edge1, edge2));
+    if(dot(meanNormal, gsNormal) < 0) gsNormal = normalize(cross(edge2, edge1));    	
+}
+
+void flatInterpolation(){
+	faceNormal();
+	for(int i = 0; i < 3; i++) {
+		gsPosition = gl_in[i].gl_Position.xyz;    
+		gl_Position = gl_in[i].gl_Position;
+		EmitVertex();
+	}
+	EndPrimitive();		
+}
+
+void phongInterpolation(){
   for(int i = 0; i < 3; i++) {
     gsNormal = tesNormal[i];
 	gsPosition = gl_in[i].gl_Position.xyz;    
@@ -21,5 +40,5 @@ void passThrough(){
 }
 
 void main() {
-	passThrough();
+	flatInterpolation();
 }
