d90ec413076d9cae11a08af2a2baad156510cd66
Laura Baakman <laurabaakman@gmail.com>
2016-01-12 11:13:52 +0100

    phongReflection in the fragment shader

--- code/pnt/resources/shaders/fragment.glsl
+++ code/pnt/resources/shaders/fragment.glsl
@@ -1,15 +1,70 @@
 #version 410 core
 
+//Definitions
+struct MaterialInfo {
+    float specularReflectionConstant;
+    float diffuseReflectionConstant;
+    float ambientReflectionConstant;
+    float alfa;
+    vec3 color;
+};
+
+struct LightInfo {
+    vec3 position;
+    vec3 ambientLightIntensity;
+    vec3 diffuseLightIntensity;
+    vec3 specularLightIntensity;
+};
+
 //Variable in
 layout(location = 0) in vec3 gsPosition;
 layout(location = 1) in vec3 gsNormal;
 
+uniform LightInfo light;
+uniform MaterialInfo material;
+
 //Variable out
 out vec4 fColor;
 
+//globals
+vec3 eye = vec3(0.0, 0.0, 0.0);
+
+void visualizeNormals(){
+    fColor = vec4((gsNormal + vec3(1)) / 2.0, 1.0);	
+}
+
+void phongReflection(){
+    vec3 viewVector = normalize(eye - gsPosition);
+    vec3 lightVector = normalize(light.position - gsPosition);
+    vec3 reflectionVector = normalize(reflect(-lightVector, gsNormal));	
+
+    vec3 ambient = frontLightMaterial(
+                material.ambientReflectionConstant,
+                material.color,
+                light.ambientLightIntensity);    
+
+    float diffuseDotProduct = dot(gsNormal, lightVector);
+    vec3 diffuse = frontLightMaterial(
+                material.diffuseReflectionConstant,
+                material.color,
+                light.diffuseLightIntensity)
+            * max(diffuseDotProduct, 0.0);    
+
+    //TODO werk if eruit        
+    vec3 specular;
+    if(diffuseDotProduct > 0){
+        specular = frontLightMaterial(
+                    material.specularReflectionConstant,
+                    material.color,
+                    light.specularLightIntensity)
+                * pow( max(dot(reflectionVector, viewVector), 0.0 ), material.alfa);
+    }
+
+    vec3 phongColor = clamp(ambient + diffuse + specular, 0, 1);         
+    fColor = vec4(phongColor, 1.0);
+}
+
 void main(void)
 {
-    // fColor = vec4(vec3(1.0, 0.5, 0.0), 1.0);
-    // fColor = vec4((gsNormal + vec3(1)) / 2.0, 1.0);
-    fColor = vec4(gsNormal, 1.0);
+    phongReflection();
 }
